d4c50a84e0a3a7954b22cf2e8c0595d36d41ce43
Luis Fernando <loiusborges@gmail.com>
2025-07-17 00:53:38 -0300

    feat/Produto: Adicionando o teste da classe produto

---
+++ test/Terminal.UnitTests/ProdutoTest.cs
@@ -0,0 +1,30 @@
+﻿using Terminal.Core;
+
+namespace Terminal.UnitTests;
+
+public class ProdutoTest
+{
+
+    [Fact]
+    public void VerificarEstoque_RetornoCorreto()
+    {
+        //Arange
+        Produto produto = new("Produto", 8.5m, 12);
+        //Act
+        var resultado = produto.VerificarEstoque();
+        //Assert
+        Assert.Equal(12, resultado);
+    }
+
+    [Fact]
+    public void ValorEstoque_RetornoCorreto()
+    {
+        //Arange
+        Produto produto = new("Produto", 8.5m, 12);
+        //Act
+        var resultado = produto.ValorEstoque();
+        //Assert
+        Assert.Equal(102, resultado);
+    }
+
+}
