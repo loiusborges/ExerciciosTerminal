6174e9272bd5fda30f3295ab2020428c7efebaf0
Luis Fernando <loiusborges@gmail.com>
2025-07-17 14:51:47 -0300

    [feat/Carro] Adicionando classe de teste Carro

---
+++ test/Terminal.UnitTests/CarroTest.cs
@@ -0,0 +1,55 @@
+﻿using Terminal.Core;
+
+namespace Terminal.UnitTests;
+
+public class CarroTest
+{
+    [Fact]
+    public void ConstrutorPadrao_DeveInicializarComValoresPadrao()
+    {
+        var carro = new Carro();
+
+        Assert.Equal("Desconhecido", carro.Marca);
+        Assert.Equal("Desconhecido", carro.Modelo);
+        Assert.Equal(0, carro.VelocidadeAtual);
+    }
+
+    [Fact]
+    public void ConstrutorParametro_DeveInicializarComValoresInformados()
+    {
+        var carro = new Carro("Ford", "Mustang");
+
+        Assert.Equal("Ford", carro.Marca);
+        Assert.Equal("Mustang", carro.Modelo);
+        Assert.Equal(0, carro.VelocidadeAtual);
+    }
+
+    [Fact]
+    public void Acelerar_DeveIncrementarVelocidade()
+    {
+        var carro = new Carro();
+        carro.Acelerar(50);
+
+        Assert.Equal(50, carro.VelocidadeAtual);
+    }
+
+    [Fact]
+    public void Frear_DeveReduzirVelocidade()
+    {
+        var carro = new Carro();
+        carro.Acelerar(60);
+        carro.Frear(20);
+
+        Assert.Equal(40, carro.VelocidadeAtual);
+    }
+
+    [Fact]
+    public void Frear_NaoDeveTerVelocidadeNegativa()
+    {
+        var carro = new Carro();
+        carro.Frear(10); 
+
+        Assert.Equal(0, carro.VelocidadeAtual);
+    }
+
+}
